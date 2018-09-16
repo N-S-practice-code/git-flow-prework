9c3bd24effae84a1df2cb3c089214983835bee76
Nico-ehs <nico.ehs05@gmail.com>
2018-09-16 16:33:38 +0000

    automatically backed up by learn

--- spec/comprehension_spec.rb
+++ spec/comprehension_spec.rb
@@ -129,7 +129,7 @@ describe "GIT" do
 
   it "18. What is the one line syntax for creating and switching to a branch called 
     'add-nav-bar'?" do
-    answer = "git checkout add-nav-bar"
+    answer = "git checkout -b add-nav-bar"
     encoded_answer = "f6317616a8601aeb64c0f27f71a4b7bf9e3b8453"
     expect(encode(answer)).to eq(encoded_answer)
   end
@@ -164,7 +164,7 @@ describe "GIT" do
       that you create: for instance, a branch made off of master will look exactly
       like a branch made off of `add-walrus-and-polar-bear`" do
     # "true" or "false", in a string
-    answer = "true"
+    answer = "false"
 
     encoded_answer = "7cb6efb98ba5972a9b5090dc2e517fe14d12cb04"
     expect(encode(answer)).to eq(encoded_answer)
